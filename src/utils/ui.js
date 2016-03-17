b419ae9059699357df4cb441363c874a9c8ec185
Rémi Becheras <rbecheras@sirap.fr>
2016-03-17 17:11:07 +0100

    add src utils

---
+++ src/utils/ui.js
@@ -0,0 +1,55 @@
+/**
+ * User interface module
+ * @module
+ * @name ui
+ * @description A module to provide configured ui elements to the plugin
+ */
+
+
+/**
+ * A hash of menu items options
+ * @var
+ * @name menuItems
+ * @type  {object}
+ *
+ */
+var menuItems = {};
+/**
+ * Insert header menu item
+ * @var
+ * @name insertHeader
+ * @type {object}
+ * @memberof menuItems
+ */
+menuItems.insertHeader = {
+  text: 'Insérer une entête',
+  icon: 'abc',
+  context: 'insert',
+  onclick: function(){
+    window.alert('insert header');
+  }
+};
+/**
+ * Insert footer menu item
+ * @var
+ * @name insertFooter
+ * @type {object}
+ * @memberof menuItems
+ */
+menuItems.insertFooter = {
+  text: 'Insérer un pied de page',
+  icon: 'abc',
+  context: 'insert',
+  onclick: function(){
+    window.alert('insert footer');
+  }
+};
+
+exports.menuItems = menuItems;
+
+exports.lockNode = function(){
+  $(this).attr('contenteditable',false);
+};
+exports.unlockNode = function(){
+  $(this).attr('contenteditable',true);
+};
