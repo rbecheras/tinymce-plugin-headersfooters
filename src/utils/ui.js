7c7176cfcc66194e1e51f92f9c92b7b063e41cf3
Rémi Becheras <rbecheras@sirap.fr>
2016-04-22 13:45:28 +0200

    use the new class MenuItem to define plugin menu items

--- src/utils/ui.js
+++ src/utils/ui.js
@@ -7,6 +7,13 @@
  * @description A module to provide configured ui elements to the plugin
  */
 
+/**
+ * Class MenuItem
+ * @var
+ * @name MenuItem
+ * @type class
+ */
+var MenuItem = require('../classes/MenuItem');
 
 /**
  * A hash of menu items options
@@ -15,39 +22,73 @@
  * @type  {object}
  *
  */
-var menuItems = {};
+var menuItems = exports.menuItems = {};
+
 /**
  * Insert header menu item
  * @var
  * @name insertHeader
- * @type {object}
+ * @type {MenuItem}
  * @memberof menuItems
  */
-menuItems.insertHeader = {
+menuItems.insertHeader = new menuItem('insertHeader',{
   text: 'Insérer une entête',
   icon: 'abc',
+  id: 'plugin-headersfooters-menuitem-insert-header',
   context: 'insert',
   onclick: function(){
     window.alert('insert header');
   }
-};
+});
+
+/**
+ * Remove header menu item
+ * @var
+ * @name removeHeader
+ * @type {MenuItem}
+ * @memberof menuItems
+ */
+menuItems.removeHeader = new MenuItem('removeHeader',{
+  text: 'Supprimer l\'entête',
+  icon: 'text',
+  context: 'insert',
+  onclick: function(){
+    window.alert('remove header');
+  }
+});
+
 /**
  * Insert footer menu item
  * @var
  * @name insertFooter
- * @type {object}
+ * @type {MenuItem}
  * @memberof menuItems
  */
-menuItems.insertFooter = {
+menuItems.insertFooter = new MenuItem('insertFooter',{
   text: 'Insérer un pied de page',
   icon: 'abc',
   context: 'insert',
   onclick: function(){
     window.alert('insert footer');
   }
-};
+});
 
-exports.menuItems = menuItems;
+/**
+ * Remove footer menu item
+ * @var
+ * @name removeFooter
+ * @type {MenuItem}
+ * @memberof menuItems
+ */
+menuItems.removeFooter = new MenuItem('removeFooter',{
+  text: 'Supprimer le pied de page',
+  icon: 'text',
+  context: 'insert',
+  onclick: function(){
+    window.alert('remove footer');
+  }
+});
+
 
 exports.lockNode = function(){
   $(this).attr('contenteditable',false);
