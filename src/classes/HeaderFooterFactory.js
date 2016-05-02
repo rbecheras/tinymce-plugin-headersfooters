915e7d1d0d19d83ecc206a7faa306452599de000
Rémi Becheras <rbecheras@sirap.fr>
2016-05-02 23:42:03 +0200

    create public methods HeaderFooterFactory.prototype.hasHeader() and HeaderFooterFactory.prototype.hasFooter()

--- src/classes/HeaderFooterFactory.js
+++ src/classes/HeaderFooterFactory.js
@@ -9,13 +9,47 @@ var Footer = require('./Footer');
  */
 function HeaderFooterFactory(editor){
   this._editor = editor;
+  this._hasHeader = false;
+  this._hasFooter = false;
 }
 
+/**
+ * Insert a new header
+ * @method
+ * @returns void
+ */
 HeaderFooterFactory.prototype.insertHeader = function(){
   this.header = new Header(this._editor,this._editor.getBody());
+  this._hasHeader = true;
 };
+
+/**
+ * Insert a new footer
+ * @method
+ * @returns void
+ */
 HeaderFooterFactory.prototype.insertFooter = function(){
   this.footer = new Footer(this._editor,this._editor.getBody());
+  this._hasFooter = true;
 };
+
+/**
+ * Check if the document has a header or not
+ * @method
+ * @returns {Boolean} true if the document has a header, false if not
+ */
+HeaderFooterFactory.prototype.hasHeader = function(){
+  return this._hasHeader;
+};
+
+/**
+ * Check if the document has a footer or not
+ * @method
+ * @returns {Boolean} true if the document has a footer, false if not
+ */
+HeaderFooterFactory.prototype.hasFooter = function(){
+  return this._hasFooter;
+};
+
 
 module.exports = HeaderFooterFactory;
