97b84fee94065755fa4cb8a7ac198c83389ca32e
Rémi Becheras <rbecheras@sirap.fr>
2016-05-02 23:40:35 +0200

    implement Header.prototype._createNode() that overrides HeadFoot.prototype._createNode()

--- src/classes/Header.js
+++ src/classes/Header.js
@@ -13,4 +13,15 @@ function Header(editor,_documentBody){
 
 Header.prototype = Object.create(HeadFoot.prototype);
 
+/**
+ * Create a new node for the header.
+ * @private
+ * @method
+ * @override HeadFoot.prototype._createNode()
+ */
+Header.prototype._createNode = function(){
+  HeadFoot.prototype._createNode.call(this);
+  $(this.node).attr('data-header',true);
+};
+
 module.exports = Header;
