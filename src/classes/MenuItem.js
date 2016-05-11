ea93c3e275e4e9e681dee4d679be66d17ff5ae44
Rémi Becheras <rbecheras@sirap.fr>
2016-05-11 17:26:06 +0200

    add jsdoc examples for MenuItem class

--- src/classes/MenuItem.js
+++ src/classes/MenuItem.js
@@ -11,15 +11,15 @@ module.exports = MenuItem
  * @param {String} name The item name
  * @param {object} options The menu item options
  * @example
-  tinymce.activeEditor.addMenuItem(new MenuItem('myAction',{
-    icon: 'text',
-    text: 'My Action',
-    visible: true,
-    disabled: true,
-    onclick: function(){
-     window.alert('overiden default onclick action')
-    }
-  }))
+ * tinymce.activeEditor.addMenuItem(new MenuItem('myAction',{
+ *   icon: 'text',
+ *   text: 'My Action',
+ *   visible: true,
+ *   disabled: true,
+ *   onclick: function(){
+ *    window.alert('overiden default onclick action')
+ *   }
+ * }))
  */
 function MenuItem (name, options) {
   this.name = name
@@ -41,10 +41,8 @@ function MenuItem (name, options) {
  * @method
  * @returns {Promise} A promise resolved by the jquery wrapper of the menu item's node element
  * @example
-  <code>
-    var menuElement = ui.menuItems.insertHeader.getUIControl()
-    menuElement.css('color','red')
-  </code>
+ * var menuElement = ui.menuItems.insertHeader.getUIControl()
+ * menuElement.css('color','red')
  */
 MenuItem.prototype.getUIControl = function () {
   var that = this
@@ -55,20 +53,30 @@ MenuItem.prototype.getUIControl = function () {
 
 /**
  * By default on click, the menu item logs on console it has been clicked and returns it to allow chainable behavior.
- * This method should be overriden after instanciation.
- * Be caution when ovveriding, please returns the menu item in the method to not break the chainable behavior allowed by the default implementation.
+ * This method should be overriden after instanciation (see example).
  * @method
  * @returns void
+ * @example
+ * // to override this placehoder callback, juste assign a new one
+ * var menuItem = new MenuItem('my menu item', options)
+ * menuItem.onclick = function () {
+ * 	// implement your own
+ * }
  */
 MenuItem.prototype.onclick = function () {
   console.info('%s menu item has been clicked', this.name)
-  return this
 }
 
 /**
  * Show the menu item UI control and returns it to allow chainable behavior.
  * @method
  * @returns {Promise} A promise resolved by the menu item
+ * @example
+ * var menuItem = new MenuItem('my menu item', options)
+ * menuItem.show().then(function (menuItem) {
+ *   // the menuItem is given as first argument
+ *   menuItem.disable() // now the menu item is shown but disabled
+ * })
  */
 MenuItem.prototype.show = function () {
   return this.getUIControl().then(function (uiControl) {
@@ -93,6 +101,18 @@ MenuItem.prototype.hide = function () {
  * Disable the menu item and returns it to allow chainable behavior.
  * @method
  * @returns {Promise} A promise resolved by the menu item
+ * @example
+ * var menuItem = new MenuItem('my menu item', options)
+ * menuItem.show().then(function (menuItem) {
+ *   // the menuItem is given as first argument
+ *   menuItem.disable().then(function (menuItem) {
+ *     // now the menu item is shown but disabled
+ *     // the menuItem is again given as first argument
+ *     setTimeout(function () {
+ *       menuItem.enable() // enable the menu item 2sec after it has been disabled
+ *     },2000)
+ *   })
+ * })
  */
 MenuItem.prototype.disable = function () {
   return this.getUIControl().then(function (uiControl) {
@@ -105,6 +125,13 @@ MenuItem.prototype.disable = function () {
  * Enable the menu item and returns it to allow chainable behavior.
  * @method
  * @returns {Promise} A promise resolved by the menu item
+ * @example
+ * var menuItem = new MenuItem('my menu item', options)
+ * menuItem.enable().then(function (menuItem) {
+ *   setTimeout(function () {
+ *     menuItem.disable() // disable the menu item 2sec after it has been enabled
+ *   },2000)
+ * })
  */
 MenuItem.prototype.enable = function () {
   return this.getUIControl().then(function (uiControl) {
@@ -122,6 +149,12 @@ MenuItem.prototype.enable = function () {
  * @private
  * @param {MenuItem} that The context for the private method
  * @returns void
+ * @example
+ * function MenuItem (name, options) {
+ *   this.name = name
+ *   _setUIControlPromise(this) // this step must be done more earlier as possible to get access to the DOMNode when it will be rendered
+ *   // continue to build the instance
+ * }
  */
 function _setUIControlPromise (that) {
   var d = q.defer()
@@ -136,15 +169,15 @@ function _setUIControlPromise (that) {
 
 /**
  * Converts a camel cased string to a dashed string
- * @function
- * @inner
+ * @method
+ * @private
+ * @memberof MenuItem
  * @param {String} inputStr The input string to dasherize
  * @example
- * <code>
-    var s = 'helloWorld'
-    camel2Dash(s)
-    // -> hello-world
-    </code>
+ * var camelCasedString = 'helloWorld'
+ * var dashedString = camel2Dash(s)
+ * console.log(dashedString)
+ * // -> hello-world
  */
 function camel2Dash (inputStr) {
   if (!inputStr.replace) throw new Error('The replace() method is not available.')
