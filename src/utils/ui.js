cd698edf9b29f52e32c6356b614f767cca141a95
Rémi Becheras <rbecheras@sirap.fr>
2016-12-10 23:56:02 +0100

    remove the obsolete create...MenuItem() methods from utils/ui

--- src/utils/ui.js
+++ src/utils/ui.js
@@ -1,7 +1,6 @@
 'use strict'
 
 var $ = window.jQuery
-var HEADER_FOOTER_ONLY_SELECTOR = 'section[data-headfoot-header], section[data-headfoot-footer]'
 
 /**
  * User interface module
@@ -10,129 +9,18 @@ var HEADER_FOOTER_ONLY_SELECTOR = 'section[data-headfoot-header], section[data-h
  * @description A module to provide configured ui elements to the plugin
  */
 
-/**
- * Class MenuItem
- * @var
- * @name MenuItem
- * @type class
- */
-var MenuItem = require('../classes/MenuItem')
-
-/**
- * A hash of menu items options
- * @var
- * @name menuItems
- * @type  {object}
- *
- */
 module.exports = {
-  createInsertHeaderMenuItem: createInsertHeaderMenuItem,
-  createRemoveHeaderMenuItem: createRemoveHeaderMenuItem,
-  createInsertFooterMenuItem: createInsertFooterMenuItem,
-  createRemoveFooterMenuItem: createRemoveFooterMenuItem,
-  createInsertPageNumber: createInsertPageNumber,
-  createinsertNumberOfPages: createinsertNumberOfPages,
   lockNode: lockNode,
   unlockNode: unlockNode,
-  addUnselectableCSSClass: addUnselectableCSSClass
-}
-
-/**
- * Create a menu item to insert a header
- * @function
- * @static
- * @returns {MenuItem}
- */
-function createInsertHeaderMenuItem () {
-  return new MenuItem('insertHeader', {
-    text: 'Insérer une entête',
-    icon: 'abc',
-    id: 'plugin-headersfooters-menuitem-insert-header',
-    context: 'insert',
-    onclick: function () {
-      window.alert('insert header')
-    }
-  })
-}
-
-/**
- * Create a menu item to remove a header
- * @function
- * @static
- * @returns {MenuItem}
- */
-function createRemoveHeaderMenuItem () {
-  return new MenuItem('removeHeader', {
-    text: "Supprimer l'entête",
-    icon: 'text',
-    context: 'insert',
-    onclick: function () {
-      window.alert('remove header')
-    }
-  })
-}
-
-/**
- * Create a menu item to insert a footer
- * @function
- * @static
- * @returns {MenuItem}
- */
-function createInsertFooterMenuItem () {
-  return new MenuItem('insertFooter', {
-    text: 'Insérer un pied de page',
-    icon: 'abc',
-    context: 'insert',
-    onclick: function () {
-      window.alert('insert footer')
-    }
-  })
-}
-
-/**
- * Create a menu item to remove a footer
- * @function
- * @static
- * @returns {MenuItem}
- */
-function createRemoveFooterMenuItem () {
-  return new MenuItem('removeFooter', {
-    text: 'Supprimer le pied de page',
-    icon: 'text',
-    context: 'insert',
-    onclick: function () {
-      window.alert('remove footer')
-    }
-  })
-}
-
-function createInsertPageNumber (editor) {
-  return new MenuItem('insertPageNumber', {
-    text: 'Insérer le numéro de page',
-    context: 'document',
-    onPostRender: function () {
-      editor.on('NodeChange', resetMenuItemState.bind(this, editor, HEADER_FOOTER_ONLY_SELECTOR))
-    },
-    cmd: 'insertPageNumberCmd'
-  })
-}
-
-function createinsertNumberOfPages (editor) {
-  return new MenuItem('insertNumberOfPages', {
-    text: 'Insérer le nombre de page',
-    // icon: 'text',
-    context: 'document',
-    onPostRender: function () {
-      editor.on('NodeChange', resetMenuItemState.bind(this, editor, HEADER_FOOTER_ONLY_SELECTOR))
-    },
-    cmd: 'insertNumberOfPagesCmd'
-  })
+  addUnselectableCSSClass: addUnselectableCSSClass,
+  resetMenuItemState: resetMenuItemState
 }
 
 /**
  * Lock a node
  * @method
- * @memberof ::callerFunction
+ * @mixin
+ * @returns {undefined}
  */
 function lockNode () {
   var $this = $(this)
@@ -143,7 +31,8 @@ function lockNode () {
 /**
  * Unlock a node
  * @method
- * @memberof ::callerFunction
+ * @mixin
+ * @returns {undefined}
  */
 function unlockNode () {
   var $this = $(this)
@@ -152,6 +41,13 @@ function unlockNode () {
   $this.focus()
 }
 
+/**
+ * Create and apply the unselectable CSS class to the active document
+ * @method
+ * @static
+ * @param {Editor} editor The tinymce active editor
+ * @returns {undefined}
+ */
 function addUnselectableCSSClass (editor) {
   var head = $('head', editor.getDoc())
   var unselectableCSSRules = '.unselectable { -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; }'
@@ -160,9 +56,13 @@ function addUnselectableCSSClass (editor) {
 }
 
 /**
-* @function
-* @inner
-*/
+ * Reset a menu item state
+ * @method
+ * @static
+ * @param {Editor} editor The tinymce active editor
+ * @param {String} selector The selector to use to DO WHAT ?
+ * @TODO finish to describe the selector parameter
+ */
 function resetMenuItemState (editor, selector) {
   var selectedElement = editor.selection.getStart()
   var $sel = $(selectedElement)
