a274f5609a346230bc75231a627b9a5519cfec6e
Rémi Becheras <rbecheras@sirap.fr>
2016-12-12 00:03:20 +0100

    create the module eventHandlers.js

---
+++ src/event-handlers.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/**
+ * This module expose the plugin event handlers
+ * @module
+ * @name eventHandlers
+ */
+
+module.exports = {
+  onInit: {},
+  onNodeChange: {},
+  onSetContent: {},
+  onBeforeSetContent: {}
+}
