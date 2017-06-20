be0b67ded164fd4a5d00375816b5d5ba2a6cdb8b
Rémi Becheras <rbecheras@sirap.fr>
2017-06-20 22:48:13 +0200

    use an object set instead of an indexed array to store Format.defaults

--- src/classes/Format.js
+++ src/classes/Format.js
@@ -8,9 +8,9 @@ function Format (name, width, height) {
   this.height = height
 }
 
-Format.defaults = []
-Format.defaults.push(new Format('A4', '210mm', '297mm'))
+Format.defaults = {}
+Format.defaults['A4'] = new Format('A4', '210mm', '297mm')
 
 if (window.env === 'development') {
-  Format.defaults.push(new Format('dev-small', '150mm', '100mm'))
+  Format.defaults['dev-small'] = new Format('dev-small', '150mm', '100mm')
 }
