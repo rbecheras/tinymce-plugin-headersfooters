46b5adb655391fb3598aa9eed6ccefc5f22f22c0
Rémi Becheras <rbecheras@sirap.fr>
2017-06-20 11:52:11 +0200

    create setStackedLayout on init callback

--- src/event-handlers.js
+++ src/event-handlers.js
@@ -9,10 +9,12 @@
 // var HeaderFooterFactory = require('./classes/HeaderFooterFactory')
 // var ui = require('./utils/ui')
 // var menuItems = require('./components/menu-items')
+var $ = window.jQuery
 
 module.exports = {
   'Init': {
-    setBodies: setBodies
+    setBodies: setBodies,
+    setStackedLayout: setStackedLayout
   },
   'NodeChange': {},
   'SetContent': {},
@@ -25,4 +27,14 @@ function setBodies (evt) {
   if (!this.documentBodies.app) {
     this.documentBodies.app = window.document.body
   }
+}
+
+function setStackedLayout (evt) {
+  this.stackedLayout.root = $('.' + this.bodyClass)
+  this.stackedLayout.wrapper = this.stackedLayout.root.children('.mce-tinymce')
+  this.stackedLayout.layout = this.stackedLayout.wrapper.children('.mce-stack-layout')
+  this.stackedLayout.menubar = this.stackedLayout.layout.children('.mce-stack-layout-item.mce-menubar.mce-toolbar')
+  this.stackedLayout.toolbar = this.stackedLayout.layout.children('.mce-stack-layout-item.mce-toolbar-grp')
+  this.stackedLayout.editarea = this.stackedLayout.layout.children('.mce-stack-layout-item.mce-edit-area')
+  this.stackedLayout.statusbar = this.stackedLayout.layout.children('.mce-stack-layout-item.mce-statusbar')
 }
