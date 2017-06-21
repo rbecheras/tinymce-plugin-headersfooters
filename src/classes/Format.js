d2eefaa4d2f9c40aff3e693c76b2794353c95f57
Rémi Becheras <rbecheras@sirap.fr>
2017-06-21 11:30:03 +0200

    refactor Format class

--- src/classes/Format.js
+++ src/classes/Format.js
@@ -2,15 +2,18 @@
 
 module.exports = Format
 
+Format.defaults = {}
+Format.defaults['A4'] = new Format('A4', '210mm', '297mm')
+
+if (window.env === 'development') {
+  Format.defaults['dev-small'] = new Format('dev-small', '150mm', '100mm')
+}
+
 function Format (name, width, height) {
   this.name = name
   this.width = width
   this.height = height
 }
 
-Format.defaults = {}
-Format.defaults['A4'] = new Format('A4', '210mm', '297mm')
 
-if (window.env === 'development') {
-  Format.defaults['dev-small'] = new Format('dev-small', '150mm', '100mm')
 }
