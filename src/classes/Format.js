b79e820f3984fee9223df36e892313266576b8cf
Rémi Becheras <rbecheras@sirap.fr>
2017-06-21 17:08:58 +0200

    implement Format.prototype.applyToPlugin()

--- src/classes/Format.js
+++ src/classes/Format.js
@@ -1,6 +1,10 @@
 'use strict'
 
+var uiUtils = require('../utils/ui')
+
 module.exports = Format
+
+Format.prototype.applyToPlugin = applyToPlugin
 
 Format.defaults = {}
 Format.defaults['A4'] = new Format('A4', '210mm', '297mm')
@@ -15,5 +19,23 @@ function Format (name, width, height) {
   this.height = height
 }
 
+function applyToPlugin (plugin) {
+  if (plugin.documentBody) {
+    var body = plugin.documentBody
+
+    applyToStackedLayout()
+  }
+
+  function applyToStackedLayout () {
+    var bodyHeight = uiUtils.getElementHeight(body)
+    var rules = {
+      height: bodyHeight,
+      minHeight: bodyHeight,
+      maxHeight: bodyHeight
+    }
+
+    plugin.stackedLayout.iframe.css(rules)
+    // console.log(body, bodyHeight, plugin.stackedLayout.iframe, uiUtils.getElementHeight(plugin.stackedLayout.iframe[0]))
+  }
 
 }
