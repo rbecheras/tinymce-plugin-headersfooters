bc94b2693efb70a88edeca0423dac0a1398c5890
Rémi Becheras <rbecheras@sirap.fr>
2017-06-21 11:31:35 +0200

    check element height on 'KeyDown Paste SetContent NodeChange'

--- src/event-handlers.js
+++ src/event-handlers.js
@@ -24,6 +24,9 @@ module.exports = {
   },
   'Blur': {
     leaveHeadFoot: leaveHeadFoot
+  },
+  'KeyDown Paste SetContent NodeChange': {
+    checkElementHeight: checkElementHeight
   }
 }
 
@@ -45,4 +48,33 @@ function enterHeadFoot (evt) {
 
 function leaveHeadFoot (evt) {
   this.disable()
+}
+
+function checkElementHeight (evt) {
+  if (this.documentBody) {
+    var win = this.editor.getWin()
+    // var $root = this.stackedLayout.root
+    // var root = $root[0]
+    var body = this.documentBody
+    // var originalHeight = uiUtils.getElementHeight(root, win)
+    var targetHeight = uiUtils.getElementHeight(body, win)
+    // console.log(root, body)
+    // console.log('root height before: ' + originalHeight)
+    // console.log('from ' + originalHeight + ' to ' + targetHeight)
+    // this.stackedLayout.root.css({height: targetHeight + ' !important'})
+    // window.jQuery([
+    //   this.stackedLayout.root,
+    //   this.stackedLayout.wrapper,
+    //   this.stackedLayout.layout,
+    //   this.stackedLayout.editarea
+    // ]).css({height: targetHeight})
+    this.stackedLayout.root.css({height: targetHeight, minHeight: targetHeight, maxHeight: targetHeight})
+    this.stackedLayout.wrapper.css({height: targetHeight, minHeight: targetHeight, maxHeight: targetHeight})
+    this.stackedLayout.layout.css({height: targetHeight, minHeight: targetHeight, maxHeight: targetHeight})
+    this.stackedLayout.editarea.css({height: targetHeight, minHeight: targetHeight, maxHeight: targetHeight})
+
+    // $root.css({height: targetHeight})
+    // var checkingHeight = uiUtils.getElementHeight(root, win)
+    // console.log('root height after: ' + checkingHeight)
+  }
 }
