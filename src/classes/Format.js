9b4b9de6ec0c44e597fc49d5132ee0dfdfd30262
Rémi Becheras <rbecheras@sirap.fr>
2017-06-25 20:26:26 +0200

    add object properties to Format class to handle document dimensions: margins, header, footer, body

--- src/classes/Format.js
+++ src/classes/Format.js
@@ -13,10 +13,53 @@ if (window.env === 'development') {
   Format.defaults['dev-small'] = new Format('dev-small', '150mm', '100mm')
 }
 
-function Format (name, width, height) {
+/**
+ * @param {string} name
+ * @param {object} config
+ */
+function Format (name, config) {
   this.name = name
-  this.width = width
-  this.height = height
+  this.width = config.width
+  this.height = config.height
+  this.margins = {
+    top: config.margins.top,
+    right: config.margins.right,
+    bottom: config.margins.bottom,
+    left: config.margins.left
+  }
+  this.header = {
+    height: config.header.height,
+    margins: {
+      right: config.header.margins.right,
+      bottom: config.header.margins.bottom,
+      left: config.header.margins.left
+    },
+    border: {
+      color: config.header.border.color,
+      style: config.header.border.style,
+      width: config.header.border.width
+    }
+  }
+  this.footer = {
+    height: config.footer.height,
+    margins: {
+      top: config.header.margins.top,
+      right: config.header.margins.right,
+      left: config.header.margins.left
+    },
+    border: {
+      color: config.header.border.color,
+      style: config.header.border.style,
+      width: config.header.border.width
+    }
+  }
+  this.body = {
+    border: {
+      color: config.body.color,
+      style: config.body.style,
+      width: config.body.width
+    }
+  }
 }
 
 function applyToPlugin (plugin) {
