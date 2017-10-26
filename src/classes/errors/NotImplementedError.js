b3e74a1fdedea949e5063a42c4d40fc3ba6c528a
Rémi Becheras <rbecheras@sirap.fr>
2017-10-26 14:10:28 +0200

    migrate NotImplementedError class to es6

--- src/classes/errors/NotImplementedError.js
+++ src/classes/errors/NotImplementedError.js
@@ -1,17 +1,12 @@
 'use strict'
 
-module.exports = NotImplementedError
-
-NotImplementedError.prototype = Object.create(Error.prototype)
-
-function NotImplementedError (functionName) {
-  var message = this.name.concat(': ', functionName, '() is not yet implemented')
-  Error.prototype.constructor.call(this, this.message)
-  this.name = 'NotImplementedError'
-  this.message = message
-  console.log('NotImplementedError stack trace (1): ', this.stack)
-  this.stack = new Error().stack
-  console.log('NotImplementedError stack trace (2): ', this.stack)
-  console.log('Debugging NotImplementedError:')
-  console.debug(this)
+export default class NotImplementedError extends Error {
+  constructor (functionName) {
+    super()
+    this.name = 'NotImplementedError'
+    this.message = this.name.concat(': ', functionName, '() is not yet implemented')
+    let stackArray = this.stack.split('\n')
+    stackArray.splice(1, 1)
+    this.stack = stackArray.join('\n')
+  }
 }
