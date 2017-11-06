4781c4da41e57202aac3a425fbbc186a6aea91f2
Rémi Becheras <rbecheras@sirap.fr>
2017-11-06 11:37:22 +0100

    create a new utils module: reflection and move isInstanceOf() from dom utils to it

---
+++ src/utils/reflection.js
@@ -0,0 +1,20 @@
+/**
+ * Tells if an object is a Node instance (from any window instance).
+ * If you want to know what is the context window where the Node constructor was used (window.Node), use the function `getNodeWindow(node)`
+ * @param {object} object any object to test
+ * @returns {boolean} true if the object is a Node instance
+ */
+export function isInstanceOf (object, className) {
+  if (typeof object !== 'object') throw new TypeError('first argument must be an object')
+  let rv
+  let _proto = object
+  do {
+    _proto = Reflect.getPrototypeOf(_proto)
+    if (_proto && _proto.constructor && _proto.constructor.name) {
+      if (_proto.constructor.name === className) {
+        rv = true
+      }
+    }
+  } while (_proto)
+  return rv
+}
