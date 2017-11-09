861b4851a443dd73fd6d3770989b1ceaf47aab2d
Rémi Becheras <rbecheras@sirap.fr>
2017-11-09 12:07:45 +0100

    implement TimeUtils.waitFor() static method

--- src/classes/utils/TimeUtils.js
+++ src/classes/utils/TimeUtils.js
@@ -15,4 +15,16 @@ export default class TimeUtils {
   static timestamp () {
     return Date.now()
   }
+
+  /**
+   * An async helper function that wait for a given time
+   * @param {Number} time The time to wait in miliseconds (ms)
+   * @return {Promise} A promise resolved in the given time
+   */
+  static async waitFor (time) {
+    if (typeof time !== 'number') throw new TypeError('first argument must be a number')
+    return new Promise(resolve => {
+      setTimeout(() => resolve(), time)
+    })
+  }
 }
