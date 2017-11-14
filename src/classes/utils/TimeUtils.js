a5752c3e9391843adb3a4448969512362939eb16
Rémi Becheras <rbecheras@sirap.fr>
2017-11-14 11:34:21 +0100

    improve TimeUtils.waitFor(time, logMassage) adding the second arg to log a specific message concated to the default one

--- src/classes/utils/TimeUtils.js
+++ src/classes/utils/TimeUtils.js
@@ -19,11 +19,15 @@ export default class TimeUtils {
   /**
    * An async helper function that wait for a given time
    * @param {Number} time The time to wait in miliseconds (ms)
+   * @param {boolean|string} logMessage True to log "wait for Xs", false to not log anything, and a given string to log and concat the given string to the initial log.
    * @return {Promise} A promise resolved in the given time
    */
-  static async waitFor (time) {
+  static async waitFor (time, logMessage) {
     if (typeof time !== 'number') throw new TypeError('first argument must be a number')
     return new Promise(resolve => {
+      if (logMessage) {
+        console.log(`Wait for ${Number(time) / 1000}s ` + logMessage)
+      }
       setTimeout(() => resolve(), time)
     })
   }
